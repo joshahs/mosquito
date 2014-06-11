2d19759b736f0bd1150031bb57cc4e1c5f382353
ilshat-fatkhullin <ilshat-fatkhullin@yandex.ru>
2014-06-11 13:00:44 +0400

    Skript

--- Mosquito/Assets/Standard Assets/Scripts/Camera Scripts/Move.cs
+++ Mosquito/Assets/Standard Assets/Scripts/Camera Scripts/Move.cs
@@ -2,10 +2,36 @@
 using System.Collections;
 
 public class Move : MonoBehaviour {
+	public Transform target;
+	public int r;
+	int x, y, x1, y1, a;
 
 	// Use this for initialization
 	void Start () {
-	
+		x = target.position.x;
+		y = target.position.y;
+		x1 = this.transform.position.x;
+		y1 = this.transform.position.y;
+
+		if (x < x1) 
+		{
+			x1 = x - (x1 - x);
+		}
+		if (x > x1) 
+		{
+			x1 = x + (x - x1);
+		}
+
+		if (y < y1) 
+		{
+			y1 = y - (y1 - y);
+		}
+		if (y > y1) 
+		{
+			y1 = y + (y - y1);
+		}
+
+		a = 
 	}
 	
 	// Update is called once per frame
